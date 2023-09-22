de4657f326ecd27ad1984b23dca99a627eda3cc7
cesar-win10-pc <ces1406@gmail.com>
2023-09-22 09:50:46 -0300

    fixing sequelize model types

--- src/model/db.js
+++ src/model/db.js
@@ -1,4 +1,4 @@
-const {Sequelize, Datatypes} = require('sequelize');
+const {Sequelize, DataTypes} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USER,process.env.DATABASE_PASS,{
     host: process.env.DATABASE_HOSTNAME,
@@ -10,36 +10,36 @@ const sequelize = new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_U
 });
 
 const Platos = sequelize.define('Plato',{
-    idPlato :{type:Datatypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
-    nombre:{type:Datatypes.STRING(40),allowNull:false},
-    descripcion:{type:Datatypes.STRING(250),allowNull:true},
-    imagen:{type:Datatypes.STRING(100)},
-    precio:{type:Datatypes.DECIMAL(8,2),allowNull:false},
-    puntaje:{type:Datatypes.INTEGER},
-    categoria:{type:Datatypes.STRING(50)},
-    ingredientes:{type:Datatypes.STRING(150)},
-    infoNutri:{type:Datatypes.STRING(150)},
-    disponible:{type:Datatypes.BOOLEAN}
+    idPlato :{type:DataTypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
+    nombre:{type:DataTypes.STRING(40),allowNull:false},
+    descripcion:{type:DataTypes.STRING(250),allowNull:true},
+    imagen:{type:DataTypes.STRING(100)},
+    precio:{type:DataTypes.DECIMAL(8,2),allowNull:false},
+    puntaje:{type:DataTypes.INTEGER},
+    categoria:{type:DataTypes.STRING(50)},
+    ingredientes:{type:DataTypes.STRING(150)},
+    infoNutri:{type:DataTypes.STRING(150)},
+    disponible:{type:DataTypes.BOOLEAN}
 },{timestamps:false, tableName:'platos'});
 
 const Mesas = sequelize.define('Mesa',{
-    idMesa:{type:Datatypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
-    capacidad:{type:Datatypes.INTEGER,allowNull:false}
+    idMesa:{type:DataTypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
+    capacidad:{type:DataTypes.INTEGER,allowNull:false}
 },{timestamps:false, tableName:'mesas'});
 
 const Comensales = sequelize.define('Comensal',{
-    idCliente :{type:Datatypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
-    nombre:{type:Datatypes.STRING(40),allowNull:false},
-    idMesa:{type:Datatypes.INTEGER}
+    idCliente :{type:DataTypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
+    nombre:{type:DataTypes.STRING(40),allowNull:false},
+    idMesa:{type:DataTypes.INTEGER}
 },{timestamps:false, tableName:'comensales'});
 
 const Pedidos = sequelize.define('Pedido',{
-    idPedido :{type:Datatypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
-    idMesa:{type:Datatypes.INTEGER},
-    idCliente:{type:Datatypes.INTEGER},
-    estado:{type:Datatypes.STRING(50)},
-    cancelable:{type:Datatypes.BOOLEAN},
-    horaPedido:{type:Datatypes.DATE}
+    idPedido :{type:DataTypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
+    idMesa:{type:DataTypes.INTEGER},
+    idCliente:{type:DataTypes.INTEGER},
+    estado:{type:DataTypes.STRING(50)},
+    cancelable:{type:DataTypes.BOOLEAN},
+    horaPedido:{type:DataTypes.DATE}
 },{timestamps:false, tableName:'pedidos'});
 
 const start = async()=>{
