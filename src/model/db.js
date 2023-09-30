f4322488247a5d7316d37f920d9e755e8d239267
cesar-win10-pc <ces1406@gmail.com>
2023-09-30 14:38:28 -0300

    actualizando tablas DB

--- src/model/db.js
+++ src/model/db.js
@@ -24,26 +24,33 @@ const Platos = sequelize.define('Plato',{
 
 const Mesas = sequelize.define('Mesa',{
     idMesa:{type:DataTypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
-    capacidad:{type:DataTypes.INTEGER,allowNull:false}
+    capacidad:{type:DataTypes.INTEGER,allowNull:false},
+    estado:{type:DataTypes.STRING(12)}
 },{timestamps:false, tableName:'mesas'});
 
 const Comensales = sequelize.define('Comensal',{
     idCliente :{type:DataTypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
-    nombre:{type:DataTypes.STRING(40),allowNull:false},
-    idMesa:{type:DataTypes.INTEGER}
+    nombre:{type:DataTypes.STRING(40),allowNull:false}
 },{timestamps:false, tableName:'comensales'});
 
 const Pedidos = sequelize.define('Pedido',{
     idPedido :{type:DataTypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
-    idMesa:{type:DataTypes.INTEGER},
-    idCliente:{type:DataTypes.INTEGER},
     estado:{type:DataTypes.STRING(50)},
+    cantidad:{type:DataTypes.INTEGER, allowNull:false},
     cancelable:{type:DataTypes.BOOLEAN},
     horaPedido:{type:DataTypes.DATE}
 },{timestamps:false, tableName:'pedidos'});
 
 const start = async()=>{
     try {
+        Comensales.hasOne(Mesas,{foreignKey:'idMesa'})
+        Mesas.belongsToMany(Comensales,{foreignKey:'idMesa'})
+        Pedidos.hasOne(Comensales,{foreignKey:'idCliente'})
+        Comensales.belongsToMany(Pedidos,{foreignKey:'idCliente'})
+        Pedidos.hasOne(Mesas,{foreignKey:'idMesa'})
+        Mesas.belongsToMany(Pedidos,{foreignKey:'idMesa'})
+        Pedidos.hasOne(Platos,{foreignKey:'idPlato'})
+        Platos.belongsToMany(Pedidos,{foreignKey:'idPlato'})
         await sequelize.authenticate();
     } catch (err) {}
 }
