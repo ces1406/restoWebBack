66e83266d172a9b2abc92954bd0dd5b753fbb306
cesar-win10-pc <ces1406@gmail.com>
2023-10-13 19:14:12 -0300

    devolviendo el idCliente

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -10,8 +10,8 @@ class UsersRoutes{
     routes(){
         this.router.get('/:nombre',async(req,res,next)=>{
             try {
-                await Comensales.create({nombre:req.params.nombre});
-                res.status(200).send({msg:'ok'})
+                const cli = await Comensales.create({nombre:req.params.nombre});
+                res.status(200).send({idCliente:cli.dataValues.idCliente})
             } catch (error) {
                 res.statusMessage=error.msj;
                 return res.status(error.code||500).send();
