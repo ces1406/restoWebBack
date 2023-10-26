6a5e520c60c53a9622d53db7f703d54e2639d677
cesar-win10-pc <ces1406@gmail.com>
2023-10-26 17:01:50 -0300

    fix

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -19,21 +19,21 @@ class UsersRoutes{
                 }
                 //cabecera.headers['Content-Type'] = 'application/json'
                 //cabecera.headers['Authorizaton'] = process.env.FCBKEY
-                let data = {
+                let body = {
                     registration_ids:[req.params.deviceid],
-                    notification: [{title:'un titulo',body:'el body es este'}],
+                    notification: {title:'un titulo',body:'el body es este'},
                     direct_boot_ok: true
                 }
                 let url = 'https://fcm.googleapis.com/fcm/send'
-                //const cli = await Comensales.create({nombre:req.params.nombre});
-
-                const rta = axios.post(url,data,config)
-                console.log('rta'+rta.response)
-                res.status(200).json({resp:JSON.stringify(rta)})
+                const cli = await Comensales.create({nombre:req.params.nombre});
+                const rta = await axios.post(url,body,config)
+                //console.log('rta.data--->',rta.data)
+                //res.status(200).json({resp:rta.data})
+                res.status(200).send({idCliente:cli.dataValues.idCliente})
             } catch (error) {
-                console.log('error-->',JSON.stringify(error))
+                //console.log('error-->',JSON.stringify(error))
                 res.statusMessage=error.msj;
-                console.log('error.msg-->',error.mesagge)
+                //console.log('error.msg-->',error.mesagge)
                 return res.status(error.code||500).send({rta:JSON.stringify(error.mesagge)});
             }
         })
