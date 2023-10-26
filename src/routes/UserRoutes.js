53ea644f7ce49e1d593ea42a1679eeb4993306be
cesar-win10-pc <ces1406@gmail.com>
2023-10-26 16:05:05 -0300

    fixing

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
 const {Comensales,start} = require('../model/db');
+const axios = require('axios')
 
 class UsersRoutes{
     constructor(){
@@ -10,23 +11,29 @@ class UsersRoutes{
     routes(){
         this.router.get('/:nombre/:deviceid',async(req,res,next)=>{
             try {
-                let cabecera = {method:'POST'}
-                cabecera.headers['Content-Type'] = 'application/json'
-                cabecera.headers['Authorizaton'] = process.env.FCBKEY
-                cabecera.body=JSON.stringify({
+                let config = {
+                    headers:{
+                        'Content-Type':'application/json',
+                        Authorization:process.env.FCBKEY
+                    }
+                }
+                //cabecera.headers['Content-Type'] = 'application/json'
+                //cabecera.headers['Authorizaton'] = process.env.FCBKEY
+                let data = {
                     registration_ids:[req.params.deviceid],
                     notification: [{title:'un titulo',body:'el body es este'}],
                     direct_boot_ok: true
-                })
+                }
                 let url = 'https://fcm.googleapis.com/fcm/send'
-                const cli = await Comensales.create({nombre:req.params.nombre});
+                //const cli = await Comensales.create({nombre:req.params.nombre});
 
-                const rta = fetch(url,cabecera)
-                //res.status(200).send({idCliente:cli.dataValues.idCliente, datos:JSON.stringify(rta)})
+                const rta = axios.post(url,data,config)
+                console.log('rta',JSON.stringify(rta))
                 res.status(200).send({resp:JSON.stringify(rta)})
             } catch (error) {
                 console.log('error-->',JSON.stringify(error))
                 res.statusMessage=error.msj;
+                console.log('error.msg-->',error.mesagge)
                 return res.status(error.code||500).send({rta:JSON.stringify(error.mesagge)});
             }
         })
