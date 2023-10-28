770f8a3d6de93fda8195dacb69a4f6937d4215b9
cesar-win10-pc <ces1406@gmail.com>
2023-10-27 21:57:26 -0300

    FIX MODEL

--- src/model/db.js
+++ src/model/db.js
@@ -31,7 +31,7 @@ const Mesas = sequelize.define('Mesa',{
 const Comensales = sequelize.define('Comensal',{
     idCliente :{type:DataTypes.INTEGER, allowNull:false, autoIncrement:true, unique:true,primaryKey:true},
     nombre:{type:DataTypes.STRING(40),allowNull:false},
-    idFcb:{type:DataTypes.INTEGER},
+    idFcb:{type:DataTypes.STRING(400)},
     llegada:{type:DataTypes.DATE},
     estado:{type:DataTypes.STRING(20)}
 },{timestamps:false, tableName:'comensales'});
